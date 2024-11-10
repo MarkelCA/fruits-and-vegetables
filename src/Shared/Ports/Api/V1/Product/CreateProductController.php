7a2769ff6deb0ce5f8e2d39ad23015cdb3db493e
Markel Cuesta <cuestaarribas.markel@proton.me>
2024-11-10 22:17:40 +0100

    Added controller to create products

---
+++ src/Shared/Ports/Api/V1/Product/CreateProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Shared\Ports\Api\V1\Product;
+
+use App\Product\Application\UseCase\SaveProductUseCase;
+use App\Product\Domain\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'v1_product_create_product', methods: ['POST'])]
+class CreateProductController extends AbstractController
+{
+
+	public function __construct(private SaveProductUseCase $useCase) {}
+
+	public function __invoke(Request $request): Response
+	{
+		$payload = json_decode($request->getContent(), true);
+
+		$product = Product::fromArray($payload);
+		$this->useCase->execute($product);
+
+		return new Response(status: Response::HTTP_CREATED);
+	}
+}
